c0dcc4144ef2a81b8e19779b378d268aaf5be958
mtpalme <72666231+mtpalme@users.noreply.github.com>
2020-10-12 21:20:08 -0400

    Add files via upload

--- ExamQuestion2.py
+++ ExamQuestion2.py
@@ -1,9 +1,15 @@
 class WorkingWithStrings:
-    # initialize string vairable for the class
-string_1 =" "
-     def __init__(self):
-        # prompt user to enter the string
-        print ('Please enter in a string')
-        string_1 = input()
+    # initialize string variable for the class
+
     def __init__(self):
-        print (string_1)
+        self.string_1= ""
+    def get_string (self):
+
+        self.string_1 = input("Enter string: ")
+    def printString(self):
+        print (self.string_1.upper())
+
+
+string_1 = WorkingWithStrings()
+string_1.get_string()
+string_1.printString()
